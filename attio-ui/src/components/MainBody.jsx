a8eccb58f7cfaed278fe4465a7209402943b2111
Winoliya Ponsingh <win.sin.rt23@dypatil.edu>
2025-02-06 20:34:35 +0530

    UI HomePage Completed

--- attio-ui/src/components/MainBody.jsx
+++ attio-ui/src/components/MainBody.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const MainBody = () => {
   return (
     <>
-      <div className="w-150 h-100 flex justify-center items-center  mt-20 ml-120 mr-120 mb-10 rounded-lg grid grid-row-4 gap-2">
+      <div className="w-150 h-100  justify-center items-center  mt-20 ml-120 mr-120 mb-10 rounded-lg grid grid-row-4 gap-2">
         <div className="flex justify-center">
           <button className="border-1 p-2 rounded-xl text-sm/5 px-8 cursor-pointer border-gray-300 hover:border-gray-700">
             How AI is revolutionizing CRM
